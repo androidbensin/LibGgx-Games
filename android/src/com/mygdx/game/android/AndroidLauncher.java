85f3bf1879acd824db098089b9af99cd12f9a2ab
Libin <android.bensin@gmail.com>
2014-09-13 12:23:00 -0400

    android annotation setup

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -9,18 +9,30 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.android.screens.StarAssault;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.libgdx_activity)
 public class AndroidLauncher extends AndroidApplication {
+
+    @ViewById(R.id.gameLayout)
+    RelativeLayout gameLayout;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+	}
+
+    @AfterViews
+    public void onSetup(){
+
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         config.useAccelerometer = false;
         config.useCompass = false;
         config.useWakelock = true;
         config.useGLSurfaceView20API18 = true;
-		View gameView = initializeForView(new StarAssault(), config);
-        View rootLayout = LayoutInflater.from(this).inflate(R.layout.libgdx_activity, null);
-        ((RelativeLayout)rootLayout.findViewById(R.id.gameLayout)).addView(gameView);
-        setContentView(rootLayout);
-	}
+        View gameView = initializeForView(new StarAssault(), config);
+        gameLayout.addView(gameView);
+    }
 }
